89a4623c16aba7004a2d37e5ea84cefaa103264b
Munevera <plojovicm3@gmail.com>
2024-03-17 05:57:50 +0100

    testing navigation

---
+++ script/navigation.js
@@ -0,0 +1,15 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+  document.body.style.overflowY = none;
+});
+
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("nav-menu");
+  })
+);
